32a6c8b55b7518f4f26f046c3bfe43eee843f1db
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-04-30 01:58:39 +0300

    inheritanceX

---
+++ InheritanceX/src/CustomerManeger.java
@@ -0,0 +1,4 @@
+
+public class CustomerManeger extends PersonManager {
+  
+}
---
+++ InheritanceX/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	CustomerManeger customerManeger=new CustomerManeger();
+	customerManeger.Add();
+	EmployeeManager employeeManager=new EmployeeManager();
+	employeeManager.BestEmployee();
+	}
+
+}
